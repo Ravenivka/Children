4769202875e525ac68cda79aced26710bb722b94
Ravenivka <raveniv@ya.ru>
2025-05-25 14:57:37 +0500

    Lesson4

---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import Typography from '@mui/material/Typography';
+
+export default function Home () {
+    return (
+        <Typography variant="h1" sx={{color: 'navy'}}>Домашняя страница</Typography>
+    )
+}
---
+++ src/Routes.js
@@ -0,0 +1,37 @@
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+
+function RouteLink() {
+  return (
+    <ButtonGroup variant="contained" aria-label="Basic button group">
+      <Button> <a href="/About">О НАС</a> </Button>
+      <Button><a href="/">Главная</a></Button>      
+    </ButtonGroup>
+  );
+}
+
+export default function MainRoute() {
+
+    return (
+        <div>
+        <RouteLink />
+        <BrowserRouter>
+            <Routes>
+                <Route exact path = "/" element={<Home />} />         
+                
+                <Route path = "/About" element={<About />} />
+                    
+                
+                <Route element={<h1 align="center">Ошибка, документ не найден</h1>} />
+                    
+                
+            </Routes>        
+        </BrowserRouter>
+        </div>
+    );
+}
